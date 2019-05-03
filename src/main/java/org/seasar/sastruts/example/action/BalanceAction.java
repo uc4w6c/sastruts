efea054f320237b6024cd855d6c6953c6de072a5
Yuta <uc4w6c@bma.biglobe.ne.jp>
2019-05-03 23:40:43 +0900

    db接続設定完了

--- src/main/java/org/seasar/sastruts/example/action/BalanceAction.java
+++ src/main/java/org/seasar/sastruts/example/action/BalanceAction.java
@@ -2,14 +2,9 @@ package org.seasar.sastruts.example.action;
 
 import java.util.List;
 
-import javax.annotation.Resource;
-
 import org.seasar.framework.container.annotation.tiger.Binding;
 import org.seasar.sastruts.example.entity.Balance;
-import org.seasar.sastruts.example.form.EchoForm;
 import org.seasar.sastruts.example.service.BalanceService;
-import org.seasar.sastruts.example.service.EchoUsersService;
-import org.seasar.struts.annotation.ActionForm;
 import org.seasar.struts.annotation.Execute;
 import org.seasar.struts.util.ResponseUtil;
 
@@ -27,7 +22,7 @@ public class BalanceAction {
 		for (Balance balance : balanceList) {
 			responseText.append(balance.toString());
 		}
-		ResponseUtil.write(responseText.toString(), "plain/text", "UTF-8");
+		ResponseUtil.write(responseText.toString(), "text/html", "UTF-8");
 		return null;
 	}
 }
