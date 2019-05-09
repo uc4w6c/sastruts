e99f7d798597121e8bc5fa2dcc96ccb4b8f5a4c3
Yuta <uc4w6c@bma.biglobe.ne.jp>
2019-05-09 22:54:34 +0900

    serviceを少しだけ進めた。Exceptionの実装方法を検討中

--- src/main/java/org/seasar/sastruts/example/service/TransferService.java
+++ src/main/java/org/seasar/sastruts/example/service/TransferService.java
@@ -1,10 +1,14 @@
 package org.seasar.sastruts.example.service;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 import org.seasar.framework.container.annotation.tiger.Binding;
 import org.seasar.sastruts.example.dao.BalanceDao;
 import org.seasar.sastruts.example.dao.TransferDao;
 import org.seasar.sastruts.example.dto.TransferResultDto;
 import org.seasar.sastruts.example.entity.Balance;
+import org.seasar.sastruts.example.entity.Transfer;
 
 public class TransferService {
 
@@ -19,8 +23,27 @@ public class TransferService {
 										String payerName,
 										long transferAmount) {
 
-		Balance balance = balanceDao.findByAccountId(payerAccountId);
+		Balance payerBalance = balanceDao.findByAccountId(payerAccountId);
+		if (payerBalance == null)
+			// Exceptionをいれること。暫定でreturn をしているだけ
+			return null;
 
+		if (transferAmount > payerBalance.amount)
+			// Exceptionを入れること。暫定return
+			return null;
+
+		Balance payeeBalance = balanceDao.findByAccountId(payerAccountId);
+		if (payeeBalance == null)
+			// Exceptionを入れること。暫定return
+			return null;
+
+		Transfer peyerTransaction = new Transfer();
+		peyerTransaction.accountId = payerAccountId;
+		peyerTransaction.name = "";
+		peyerTransaction.transferAmount = -transferAmount;
+		// peyerTransaction.transferDate = new LocalDate(); // 日付型を確認したい
+
+		Transfer payeeTransaction = new Transfer();
 
 		return null;
 	}
