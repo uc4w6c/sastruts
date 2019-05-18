d7d4164bcbecf8bbf1420fc8a9df8afbf1c02e15
Yuta <uc4w6c@bma.biglobe.ne.jp>
2019-05-18 23:29:59 +0900

    junit試す

---
+++ src/test/java/org/seasar/sastruts/example/entity/IsTransferResultDto.java
@@ -0,0 +1,21 @@
+package org.seasar.sastruts.example.entity;
+
+import org.hamcrest.BaseMatcher;
+import org.hamcrest.Description;
+import org.seasar.sastruts.example.dto.TransferResultDto;
+
+public class IsTransferResultDto extends BaseMatcher<TransferResultDto> {
+
+	@Override
+	public boolean matches(Object arg0) {
+		// TODO 自動生成されたメソッド・スタブ
+		return false;
+	}
+
+	@Override
+	public void describeTo(Description arg0) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+	}
+
+}
